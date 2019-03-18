98e0949daffa15d73f6acfaa5515c76abfdb662d
Carolina <content00web@gmail.com>
2019-03-18 11:51:07 -0500

    updating include

--- contentosite/urls.py
+++ contentosite/urls.py
@@ -6,5 +6,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include("posts.urls", namespace = 'posts')),
+    url(r'', include(("posts.urls", "posts_url"), namespace = 'posts')),
 ]
